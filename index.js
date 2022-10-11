37c22c7178223d196c3cd63def679af0b9a13b89
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-10-12 00:03:38 +0600

    delte method add

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = "mongodb+srv://anas:AYEx3Rv6xtEZVVze@cluster0.v3q4mcg.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
     await client.connect();
     const collectionOne = client.db("anas").collection("projects");
     const collectionTwo = client.db("anas").collection("members");
+    const collectionUsers = client.db("anas").collection("users");
     console.log('connected to mongodb');
+
+    const verifyAdmin = async (req, res, next) => {
+        const requester = req.decoded.email;
+        const requseterAccount = await collectionUsers.findOne({ email: requester });
+        if (requseterAccount.role === 'admin') {
+            next();
+        }
+        else {
+            res.status(403).send({ message: 'forbidden' });
+        }
+    }
+
     try {
         // get the data from mongodb
         app.get('/details', async (req, res) => {
@@ -31,6 +44,18 @@ async function run() {
             res.send(results);
         });
 
+        app.get('/members/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const mems = await collectionTwo.findOne(query);
+            res.send(mems);
+
+        });
+
+        app.get('/users', async (req, res) => {
+            const users = await collectionUsers.find().toArray();
+            res.send(users);
+        })
 
 
 
@@ -42,6 +67,16 @@ async function run() {
             const result = await collectionTwo.insertOne(newMember);
             res.send(result);
         });
+
+
+        // DELETE
+        app.delete('/members/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await collectionTwo.deleteOne(query);
+            res.send(result);
+        });
+
 
 
     } finally {
